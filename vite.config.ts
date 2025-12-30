f14b33e8910e2e1d26c8cba48f82c4aa768903fc
Akifbade <ACSHONA143@GMAIL.COM>
2025-12-30 12:51:52 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // This allows the app to access the API_KEY provided in Vercel settings
+    'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
+  },
   build: {
     outDir: 'dist',
+    target: 'esnext'
   },
   server: {
     port: 3000,
